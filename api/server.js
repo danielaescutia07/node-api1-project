3ebc9c27121b5013bffd0b015b016687b9df9f67
danielaescutia07 <danielaescutia@yahoo.com>
2022-03-30 17:21:03 -0500

    wip

--- api/server.js
+++ api/server.js
@@ -1,3 +1,43 @@
 // BUILD YOUR SERVER HERE
+const express = require('express');
+const User = require('./users/model');
+const server = express();
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+server.get('/api/users', async (req, res) => {
+  try {
+    const users = await User.find()
+    res.json(users)
+  } catch (err) {
+    res.status(500).json({
+      message: 'User not found',
+      error: err.message
+    })
+  }
+});
+
+server.get('/api/users/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    const user = await User.findById(id);
+    if (!user) {
+      res.status(404).json({
+        message: 'The user with the specified ID does not exist'
+      })
+    } else {
+      res.json(user);
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: `user with id ${id} not found`,
+      error: err.message
+    })
+  }
+});
+
+server.use('*', (req, res) => {
+  res.status(404).json({
+    message: 'not found'
+  });
+});
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
